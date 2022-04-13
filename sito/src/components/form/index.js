7516707d497db187426b53c9656d814e3deafe5c
cezardaniel <dantefigliodisparta@gmail.com>
2022-04-14 01:25:11 +0200

    Modifica
    aggiunto img e altre cacate

--- sito/src/components/form/index.js
+++ sito/src/components/form/index.js
@@ -1,7 +1,9 @@
 import React, {useState } from "react";
 import ReactDOM from "react-dom";
 import './styles/style.css';
-
+import { FiUser } from "react-icons/fi";
+import { BiLockAlt } from "react-icons/bi";
+import { Input,Label} from "semantic-ui-react";
 function Form(){
   // React States
   const [errorMessages, setErrorMessages] = useState({});
@@ -54,22 +56,32 @@ function Form(){
 
   // JSX code for login form
   const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
+    <div className="box">
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+           
+          <Input labelPosition='right' type='text' placeholder='Amount'>
+            <Label basic><FiUser></FiUser>
+            <input type="text" name="uname" placeholder="Username" required></input>
+            </Label>
+          </Input>
           {renderErrorMessage("uname")}
-        </div>
-        <div className="input-container">
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
+          </div>
+          <div className="input-container">
+            <Input labelPosition='right' type='text' placeholder='Amount'>
+              <Label basic><BiLockAlt></BiLockAlt>
+              <input type="password" name="pass" placeholder="Password" required />
+              </Label>
+            </Input>
+            
+            {renderErrorMessage("pass")}
+          </div>
+          <div className="button-container">
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 
